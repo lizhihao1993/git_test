ad8fed4446f300524e6e8cfec04155df039795db
lizhihao <lizhihao@aibaoxian.com>
2018-05-14 17:40:21 +0800

    dev 提交Hello ，去掉！！！

--- springboot_demo/src/test/java/com/example/demo/HelloWorld.java
+++ springboot_demo/src/test/java/com/example/demo/HelloWorld.java
@@ -8,4 +8,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    public void say(){
+        System.out.println("I don't say");
+    }
 }
