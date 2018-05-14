92a55875f9039037b04608221e4ae977c8060860
lizhihao <lizhihao@aibaoxian.com>
2018-05-14 17:38:33 +0800

    master初始化

---
+++ springboot_demo/src/test/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * @author lizhihao [lizhihao@aibaoxian.com]
+ * @date 2018/5/14.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
