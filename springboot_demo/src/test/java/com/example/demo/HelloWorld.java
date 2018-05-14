38d58acd161d0c94757f3374a4059aa1d51a90bf
lizhihao <lizhihao@aibaoxian.com>
2018-05-14 17:42:03 +0800

    bugfix, main进行修改

--- springboot_demo/src/test/java/com/example/demo/HelloWorld.java
+++ springboot_demo/src/test/java/com/example/demo/HelloWorld.java
@@ -6,6 +6,7 @@ package com.example.demo;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world !!!!  ---- bugfix");
     }
+
 }
