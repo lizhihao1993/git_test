fab6f78634f2a544b4febc6af18f56c58bf648f1
zhuoguojiang <zhuoguojiang@aibaoxian.com>
2018-05-14 18:17:58 +0800

    other dev

--- springboot_demo/src/test/java/com/example/demo/HelloWorld.java
+++ springboot_demo/src/test/java/com/example/demo/HelloWorld.java
@@ -6,7 +6,7 @@ package com.example.demo;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world !!!!  ---- bugfix");
+        System.out.println("hello world !!!!  ---- bugfix ------xxxxx other");
     }
 
     public void say(){
